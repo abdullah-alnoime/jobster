250b07d6d4b7ab26d1046711cefd7566cf90b5b7
Abdullah Alnoime <abdullahalnoime@gmail.com>
2025-06-28 19:39:56 +0300

    update

--- app.js
+++ app.js
@@ -5,12 +5,21 @@ const { notFound, handleError, authorization } = require("./middlewares");
 const connect = require("./db/connect");
 const cors = require("cors");
 require("dotenv").config();
+const helmet = require("helmet");
+const rateLimit = require("express-rate-limit");
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI;
 
+app.use(limiter);
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 app.use(express.static("./public"));
 app.use("/api/v1/auth", auth);
 app.use("/api/v1/jobs", authorization, jobs);
